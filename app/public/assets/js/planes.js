f310d3c78da1992fa88aacbae3d293ae59c8a132
tophluck <steve88bb@gmail.com>
2018-05-27 23:25:33 -0400

    updated planes.js file and related code

--- app/public/assets/js/planes.js
+++ app/public/assets/js/planes.js
@@ -23,4 +23,63 @@ $(function() {
             });
         });
     });
+    $(".approve").on("click", function(event) {
+        event.preventDefault();
+        var code = $(this).data("code");
+        var action = {
+            approvalStatus: true
+        }
+        $.ajax("/api/flight_info/" + code, {
+            type: "PUT",
+            data: action
+        }).then(function() {
+            location.reload();
+        });
+    });
+    $(".notApprove").on("click", function(event) {
+        event.preventDefault();
+        var code = $(this).data("code");
+        var action = {
+            approvalStatus: false
+        }
+        $.ajax("/api/flight_info/" + code, {
+            type: "PUT",
+            data: action
+        }).then(function() {
+            location.reload();
+        });
+    });
+    $(".moreInfo").on("click", function(event) {
+        event.preventDefault();
+        var code = $(this).data("code");
+        $.ajax("/" + code, {
+            type: "GET",
+            data: "",
+            success: function(data){
+                $("#moreInfoModalBody").html(data);
+                $("#moreInfoModal").modal('show');
+            }
+        }).then(function() {
+            // create javascript that makes popup window to show full detailed information on flight here.
+        });
+    });
+    $(".weather").on("click", function(event) {
+        event.preventDefault();
+        var code = $(this).data("code");
+        $.ajax("/flight_plan/" + code, {
+            type: "GET",
+            url: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2626d7eb76d0259ea5d3e2117882a27d"
+        }).then(function(data) {
+            $.ajax("weather api address here", {
+                type: "GET",
+                data: "",
+                success: function(data){
+                    $("#weatherModalBody").html(data);
+                    $("#weatherModal").modal('show');
+                }
+            }).then(function(weather) {
+                // create popup window using weather api data and coordinates from data
+            });
+        });
+    });
 });
